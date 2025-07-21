368103e1e86362c5f549bfff400988be09df6c98
Kartik <kkarnayil@gmail.com>
2025-07-21 20:07:05 +0530

    Initial code setup

---
+++ src/main/java/com/codewithkartik/kstore/KstoreApplication.java
@@ -0,0 +1,13 @@
+package com.codewithkartik.kstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KstoreApplication.class, args);
+	}
+
+}
