9445230e1b80f67fa19e920f7fee4b51bdda977b
Kartik <kkarnayil@gmail.com>
2025-07-22 09:45:09 +0530

    Added notification service as dependency and used in project

--- src/main/java/com/codewithkartik/kstore/KstoreApplication.java
+++ src/main/java/com/codewithkartik/kstore/KstoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.codewithkartik.kstore", "com.codewithkartik.knotification"})
 public class KstoreApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/codewithkartik/kstore/services/impl/OrderServiceImpl.java
+++ src/main/java/com/codewithkartik/kstore/services/impl/OrderServiceImpl.java
@@ -2,16 +2,21 @@ package com.codewithkartik.kstore.services.impl;
 
 import com.codewithkartik.kstore.services.OrderService;
 import com.codewithkartik.kstore.services.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import com.codewithkartik.knotification.NotificationManager;
 
 @Service("orderService")
 public class OrderServiceImpl implements OrderService {
 
     private final PaymentService paymentService;
 
-    public OrderServiceImpl(@Qualifier("paypal") PaymentService paymentService) {
+    private final NotificationManager notificationManager;
+
+    public OrderServiceImpl(@Qualifier("paypal") PaymentService paymentService, @Autowired NotificationManager notificationManager) {
         this.paymentService = paymentService;
+        this.notificationManager = notificationManager;
     }
 
     @Override
@@ -19,6 +24,7 @@ public class OrderServiceImpl implements OrderService {
         // Logic to place an order
         System.out.println("Placing order: " + orderDetails);
        this.paymentService.processPayment("10 $");
+       this.notificationManager.sendNotification("Order placed successfully");
     }
 
 }
